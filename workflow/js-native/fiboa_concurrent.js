94295fe1dfdb35254a6cf4810ef64a15e1db446f
Tomáš Olvecký <tomas.olvecky@gmail.com>
2026-05-25 19:18:24 +0200

    refactor: Upgrade to obelisk 0.38.0-rc.2

--- workflow/js-native/fiboa_concurrent.js
+++ workflow/js-native/fiboa_concurrent.js
@@ -1,13 +1,13 @@
+import { fiboSubmit, fiboAwaitNext } from 'benchmark-fibo:activity-obelisk-ext/fiboa';
+
 export default function fiboa_concurrent(n, iterations) {
     const js = obelisk.createJoinSet();
     for (let i = 0; i < iterations; i++) {
-        js.submit('benchmark-fibo:activity/fiboa.fibo', [n]);
+        fiboSubmit(js, n);
     }
     let last = 0;
     for (let i = 0; i < iterations; i++) {
-        const response = js.joinNext();
-        if (!response.ok) throw 'activity failed';
-        last = obelisk.getResult(response.id).ok;
+        last = fiboAwaitNext(js);
     }
     return last;
 }
